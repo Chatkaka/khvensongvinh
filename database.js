58fdf115fae9541844bd634d1d62c49b21f7a339
Chatkaka <hochat.tayan@gmail.com>
2026-07-25 23:20:27 +0700

    Upload database.js

--- database.js
+++ database.js
@@ -7717,7 +7717,7 @@ const INITIAL_DATABASE = {
             "goi_thau": "Tất cả các gói"
         }
     ],
-    "last_updated": "2026-07-17 09:24:42",
+    "last_updated": "2026-07-25 23:19:51",
     "telegram_config": {
         "bot_token": "",
         "chat_id": "-1004313859012"
